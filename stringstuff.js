1ce603c4abde0463a1c3fda4729836d46993c395
Ashish Mukharji <ashish@ashish.org>
2021-10-17 01:27:36 -0700

    Directly open new URL; deprecate: copy text to clipboard

--- stringstuff.js
+++ stringstuff.js
@@ -1,11 +1,17 @@
 var inputFEN = prompt("Please enter FEN: ");
 	var f1 = inputFEN.replaceAll('/','U');
 	var f2 = f1.replaceAll(" ","%20");
-	var outputURL = "chessable.com/courses/fen/" + f2;
-	document.getElementById("output").innerHTML = outputURL;
+	var outputURL = "https://chessable.com/courses/fen/" + f2;
 
-	function myFunction() {
-		navigator.clipboard.writeText(outputURL);
-		alert("Copied the text: " + outputURL);
-	}
+	// To display new URL onscreen
+	// document.getElementById("output").innerHTML = outputURL; 
+
+	// Open a new window!
+	window.location.assign(outputURL);
+
+	// No longer going the lengthy clipboard route
+	// function myFunction() {
+	// 	navigator.clipboard.writeText(outputURL);
+	// 	alert("Copied the text: " + outputURL);
+	// } 
 
